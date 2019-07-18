8faeda16da4a39d2c4c2ff7f64773843c4ea0c3b
Tawanda Cooper <tawandacooper31@gmail.com>
2019-07-18 14:25:57 -0500

    Week 3 Homework

---
+++ U1-M3-Summative-Cooper-Tawanda/src/main/java/com/company/U1M3SummativeCooperTawanda/U1M3SummativeCooperTawandaApplication.java
@@ -0,0 +1,13 @@
+package com.company.U1M3SummativeCooperTawanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U1M3SummativeCooperTawandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U1M3SummativeCooperTawandaApplication.class, args);
+	}
+
+}
